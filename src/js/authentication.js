b7af7b094e3f8fd10af777f9683f9a6a5c0bdfc8
mikaelangelo12 <mikaelangelo12@gmail.com>
2021-08-27 23:24:00 -0300

    Iniciando a Criação adicionando o Firebase

---
+++ src/js/authentication.js
@@ -0,0 +1,8 @@
+var email = document.getElementById('email')
+var senha = document.getElementById('senha')
+var btnEntrarConta = document.getElementById('loginConta');
+var btnCriarConta = document.getElementById('criarLogin')
+
+
+
+
